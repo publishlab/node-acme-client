358c7395f7a3f46dcf8faaa0289f3abd9dcec227
Nicholas Morsman <nicholas@publishlab.com>
2019-08-21 15:05:21 +0200

    Clean up HTTP client when signing request payload

--- src/http.js
+++ src/http.js
@@ -141,10 +141,11 @@ class HttpClient {
      * @param {string} url Request URL
      * @param {object} payload Request payload
      * @param {string} [nonce] Request nonce
+     * @param {string} [kid] Request KID
      * @returns {Promise<object>} Signed HTTP request body
      */
 
-    async createSignedBody(url, payload, nonce = null, kid = null) {
+    async createSignedBody(url, payload = null, nonce = null, kid = null) {
         /* JWS header */
         const header = {
             url,
@@ -165,19 +166,10 @@ class HttpClient {
         }
 
         /* Request payload */
-        let result;
-        if (payload === '') {
-            result = {
-                payload: '',
-                protected: util.b64encode(JSON.stringify(header))
-            };
-        }
-        else {
-            result = {
-                payload: util.b64encode(JSON.stringify(payload)),
-                protected: util.b64encode(JSON.stringify(header))
-            };
-        }
+        const result = {
+            payload: payload ? util.b64encode(JSON.stringify(payload)) : '',
+            protected: util.b64encode(JSON.stringify(header))
+        };
 
         /* Signature */
         const signer = crypto.createSign('RSA-SHA256').update(`${result.protected}.${result.payload}`, 'utf8');
@@ -195,7 +187,7 @@ class HttpClient {
      * @param {string} url Request URL
      * @param {string} method HTTP method
      * @param {object} payload Request payload
-     * @param {string} [kid] KID
+     * @param {string} [kid] Request KID
      * @returns {Promise<object>} HTTP response
      */
 
