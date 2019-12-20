9c03330c006fcf377f39cf8ca003f72433ca4745
Nicholas Morsman <nicholas@publishlab.com>
2019-12-20 15:39:58 +0100

    Add augmented ACME interfaces to type definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -17,6 +17,19 @@ export type CsrString = string;
 
 
 /**
+ * Augmented ACME interfaces
+ */
+
+export interface Order extends rfc8555.Order {
+    url: string;
+}
+
+export interface Authorization extends rfc8555.Authorization {
+    url: string;
+}
+
+
+/**
  * Client
  */
 
@@ -31,8 +44,8 @@ export interface ClientOptions {
 
 export interface ClientAutoOptions {
     csr: CsrBuffer | CsrString;
-    challengeCreateFn: (authz: rfc8555.Authorization, challenge: rfc8555.Challenge, keyAuthorization: string) => Promise<any>;
-    challengeRemoveFn: (authz: rfc8555.Authorization, challenge: rfc8555.Challenge, keyAuthorization: string) => Promise<any>;
+    challengeCreateFn: (authz: Authorization, challenge: rfc8555.Challenge, keyAuthorization: string) => Promise<any>;
+    challengeRemoveFn: (authz: Authorization, challenge: rfc8555.Challenge, keyAuthorization: string) => Promise<any>;
     email?: string;
     termsOfServiceAgreed?: boolean;
     skipChallengeVerification?: boolean;
@@ -46,15 +59,15 @@ export class Client {
     createAccount(data?: rfc8555.AccountCreateRequest): Promise<rfc8555.Account>;
     updateAccount(data?: rfc8555.AccountUpdateRequest): Promise<rfc8555.Account>;
     updateAccountKey(newAccountKey: PrivateKeyBuffer | PrivateKeyString, data?: object): Promise<rfc8555.Account>;
-    createOrder(data: rfc8555.OrderCreateRequest): Promise<rfc8555.Order>;
-    finalizeOrder(order: rfc8555.Order, csr: CsrBuffer | CsrString): Promise<rfc8555.Order>;
-    getAuthorizations(order: rfc8555.Order): Promise<rfc8555.Authorization[]>;
-    deactivateAuthorization(authz: rfc8555.Authorization): Promise<rfc8555.Authorization>;
+    createOrder(data: rfc8555.OrderCreateRequest): Promise<Order>;
+    finalizeOrder(order: Order, csr: CsrBuffer | CsrString): Promise<Order>;
+    getAuthorizations(order: Order): Promise<Authorization[]>;
+    deactivateAuthorization(authz: Authorization): Promise<Authorization>;
     getChallengeKeyAuthorization(challenge: rfc8555.Challenge): Promise<string>;
-    verifyChallenge(authz: rfc8555.Authorization, challenge: rfc8555.Challenge): Promise<boolean>;
+    verifyChallenge(authz: Authorization, challenge: rfc8555.Challenge): Promise<boolean>;
     completeChallenge(challenge: rfc8555.Challenge): Promise<rfc8555.Challenge>;
-    waitForValidStatus<T = rfc8555.Order | rfc8555.Authorization | rfc8555.Challenge>(item: T): Promise<T>;
-    getCertificate(order: rfc8555.Order): Promise<string>;
+    waitForValidStatus<T = Order | Authorization | rfc8555.Challenge>(item: T): Promise<T>;
+    getCertificate(order: Order): Promise<string>;
     revokeCertificate(cert: CertificateBuffer | CertificateString, data?: rfc8555.CertificateRevocationRequest): Promise<void>;
     auto(opts: ClientAutoOptions): Promise<string>;
 }
